93370c11d680e185277c636086c7f75dffe44726
maccree <maccree.a@yandex.ru>
2020-10-06 22:13:17 +0300

    decision: 1 Input/Output - 'Sum of three numbers'

---
+++ solvedtask/1_input_output/sumnumbers.py
@@ -0,0 +1,11 @@
+#-- coding: utf-8 --
+
+print('a + b + c = x')
+
+a_num = int(input('a = ')) # entering a number 'a'
+b_num = int(input('b = ')) # entering a number 'b'
+c_num = int(input('c = ')) # entering a number 'c'
+
+x = a_num + b_num + c_num # calculating the sum of a b c
+
+print('x = ' + str(x))  # output x
