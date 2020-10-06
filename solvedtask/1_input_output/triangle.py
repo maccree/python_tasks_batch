ee84b16c8e3d3a35d446c4b638ff7e10f5374c7d
maccree <maccree.a@yandex.ru>
2020-10-06 22:28:41 +0300

    decision: 1 Input/Output - 'Area of a right triangle'

---
+++ solvedtask/1_input_output/triangle.py
@@ -0,0 +1,24 @@
+#-- coding: utf-8 --
+
+
+print('S = 1/2*bh')
+b = int(input('b = '))
+h = int(input('h = '))
+
+
+pr_s = b * h % 2 
+
+s1 = b * h // 2
+s2 = b * h / 2
+
+if pr_s == 0:
+    print('S = ' + str(s1))
+elif pr_s != 0:
+    print('S = ' + str(s2))
+else:
+    print('ERROR')
+
+#print('S = ' + str(s))
+
+
+
