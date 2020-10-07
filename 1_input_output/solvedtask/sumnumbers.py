b8fac792c57cc3ec81ed357b9e33c279b0a5673a
maccree <maccree.a@yandex.ru>
2020-10-07 21:49:14 +0300

    design changes for Alexey

--- 1_input_output/solvedtask/sumnumbers.py
+++ 1_input_output/solvedtask/sumnumbers.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-
 print('a + b + c = x')
 
-a_num = int(input('a = ')) # entering a number 'a'
-b_num = int(input('b = ')) # entering a number 'b'
-c_num = int(input('c = ')) # entering a number 'c'
 
-x = a_num + b_num + c_num # calculating the sum of a b c
+# entering a number 'a'
+a = int(input('a = '))
+# entering a number 'b'
+b = int(input('b = '))
+ # entering a number 'c'
+c = int(input('c = ')) 
+
+# Calculating the sum of 'a', 'b' and 'c'
+x = a + b + c
 
 print('x = ' + str(x))  # output x
--- 1_input_output/solvedtask/triangle.py
+++ 1_input_output/solvedtask/triangle.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
 
-
-
 print('S = 1/2*bh')
+
+'''
+'b' -  is one of catite
+'h' is the height, but for a right-angled triangle it is also a catite that is opposite to 'b'
+'''
+
 b = int(input('b = '))
 h = int(input('h = '))
 
+# Selecting a fractional part from an area, this is necessary for the next block
+pr_s = b * h % 2
 
-pr_s = b * h % 2 
+# Calculating the area without a fractional part, you need for a beautiful output
+s = b * h // 2
 
-s1 = b * h // 2
-s2 = b * h / 2
+# Calculating the area with a fractional remainder
+s_withresidue = b * h / 2
 
+
+# Check whether there is a fractional remainder
+# If it is not, that is, it is equal to 0, then output the variable 's'
 if pr_s == 0:
-    print('S = ' + str(s1))
+    print('S = ' + str(s))
+# If the remainder is, that is, not equal to 0, then output the variable 's_withresidue'
 elif pr_s != 0:
-    print('S = ' + str(s2))
+    print('S = ' +  str(s_withresidue))
+# If neither the first nor the second item is executed(which can't be), output 'ERROR'
 else:
     print('ERROR')
-
-#print('S = ' + str(s))
-
-
-
