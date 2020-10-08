de64d1bcb1adcd0b3df0d276b629579486e61f68
maccree <maccree.a@yandex.ru>
2020-10-08 19:23:29 +0300

    next and previous

---
+++ 1_input_output/nexnprevious.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+numb = int(input('Your number: '))
+
+next = numb + 1
+previous = numb - 1
+
+print('The next number for the number ' + str(numb) + ' is ' + str(next) + '. The previous number for the number ' + str(numb) + ' is ' + str(previous) + '.' )
