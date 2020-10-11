0f502125885e22e5a849b165a3c2ec1c69af0737
maccree <maccree.a@yandex.ru>
2020-10-11 14:46:59 +0300

    Input/Output - Hello, Harry

--- 1_input_output/hello.py
+++ 1_input_output/hello.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+'''
+We ask the user for a name and write it to the 'name' variable with the str type. And output the message 'Hello, <name>'
+'''
+
 name = str(input('Your name? '))
 
 print('Hello, ' , name)
