682f25b4cb92bfa6e11dc59af194523b6cfb527c
maccree <maccree.a@yandex.ru>
2020-10-11 14:44:24 +0300

    Input/Output - Dasks

--- 1_input_output/dasks.py
+++ 1_input_output/dasks.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+'''We ask the user how many students there are in three classes(we write everything in the variables class1, class2 and class3).
+Then divide the students in each class by 2(since there are 2 people sitting at the same Desk), and if it is not divided by 2, then add one more Desk'''
 
 
 class1 = int(input('Сколько человек в первом классе? '))
